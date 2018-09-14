8699ad8858f6692b8b499a464bd65cb6ad421c9e
MattBelan <mrb3617@rit.edu>
2018-09-14 19:57:59 -0400

    Potion Throwing
    Balls (representing potions) are thrown where the player is looking. Potions do not interact with anything yet.

---
+++ AetherAlchemy/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+	//player references and variables
+	public GameObject potion;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			ThrowPotion();
+		}
+	}
+
+	void ThrowPotion(){
+		Instantiate (potion, this.gameObject.transform);
+	
+	}
+}
---
+++ AetherAlchemy/Assets/Scripts/PotBehavior.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotBehavior : MonoBehaviour {
+
+	//potion variables
+	public Vector3 potPos;
+	public Vector3 direction;
+	public Vector3 velocity;
+	public Vector3 acceleration;
+
+	public float speed;
+	//
+	public Vector3 gravity;
+
+	// Use this for initialization
+	void Start () {
+		potPos = transform.position;
+		potPos.y -= .35f;
+		direction = GameObject.Find ("FirstPersonCharacter").transform.forward;
+		direction.Normalize ();
+		velocity = speed * direction;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ApplyGravity ();
+
+
+		velocity += acceleration * Time.deltaTime;
+
+		potPos += velocity * Time.deltaTime;
+
+		direction = velocity.normalized;
+
+		acceleration = Vector3.zero;
+		transform.position = potPos;
+	}
+
+	void ApplyGravity(){
+		acceleration += gravity;
+	
+	}
+}
