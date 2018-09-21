4d0e1ca14bb7e5fbe5174292031b2e0a85e0a11c
MattBelan <mrb3617@rit.edu>
2018-09-20 23:04:46 -0400

    Burning
    Burnable objects burn. The final puzzle is a bit weird since the collider for the seesaw doesn't move with the object, so I'll continue working on that.

---
+++ AetherAlchemy/Assets/Scripts/BurnableScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BurnableScript : MonoBehaviour {
+
+    GameObject burnablePotion;
+    Collider burnCollider;
+    Collider potCollider;
+
+	// Use this for initialization
+	void Start () {
+        burnCollider = GetComponent<Collider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        burnablePotion = GameObject.Find("PotionTest(Clone)");
+        if (burnablePotion != null)
+        {
+            potCollider = burnablePotion.GetComponent<Collider>();
+        }
+        if (CheckIntersection())
+        {
+            Destroy(burnablePotion);
+            Destroy(gameObject);
+        }
+	}
+
+    bool CheckIntersection()
+    {
+        if (burnablePotion == null)
+        {
+            return false;
+        }
+        if (burnCollider.bounds.Intersects(potCollider.bounds))
+        {
+            return true;
+        }
+        return false;
+    }
+}
