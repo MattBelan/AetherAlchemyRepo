2cfbcca35eb7ec328f19213157670a95cce70bd0
LeoHolman <lah2719@g.rit.edu>
2018-11-03 13:42:20 -0400

    Tweaks and changes from 11/3 meeting

--- AetherAlchemy/Assets/Scripts/AdjustAudio.cs
+++ AetherAlchemy/Assets/Scripts/AdjustAudio.cs
@@ -1,16 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class AdjustAudio : MonoBehaviour {
+	public Slider musicVolumeSlider;
+	public Slider fxVolumeSlider;
+	public AudioMixer soundMixer;
 
-	// Use this for initialization
-	void Start () {
+
+	void Start() {
+		float fxValue;
+		bool fxResult =  soundMixer.GetFloat("fxVolume", out fxValue);
+		fxVolumeSlider.value = fxValue;
+		float musicValue;
+		bool musicResult = soundMixer.GetFloat("musicVolume", out musicValue);
+		musicVolumeSlider.value = musicValue;
+		Debug.Log(musicVolumeSlider.value);
+		Debug.Log(musicVolumeSlider.value);
 
 	}
 
-	// Update is called once per frame
-	void Update () {
+	public void GetVolume(){
+		float value;
+				bool result =  soundMixer.GetFloat("fxVolume", out value);
+				Debug.Log(result);
+				Debug.Log(value);
 
-	}
+				// if(result){
+				//
+				// 		return value;
+				// }else{
+				// 		return 0f;
+				// }
+		}
+
+
+
+	// public void ChangeVolume() {
+	// 	musicMixer.SetFloat(musicVolumeSlider.value);
+	// 	fxMixer.SetFloat(fxVolumeSlider.value);
+	// }
 }
