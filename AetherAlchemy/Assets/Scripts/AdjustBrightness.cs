e3082272cab0b90863a64719bc97e3ac7c517112
LeoHolman <lah2719@g.rit.edu>
2018-11-05 13:13:57 -0500

    Add script to adjust brightness- bugged
    Script should adjust brightness but does not, needs
    more testing

---
+++ AetherAlchemy/Assets/Scripts/AdjustBrightness.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdjustBrightness : MonoBehaviour {
+
+	public Slider brightnessSlider;
+	public float newBrightness;
+	// Use this for initialization
+	void Start () {
+		Debug.Log(RenderSettings.ambientLight);
+	}
+
+	// Update is called once per frame
+	public void ChangeBrightness () {
+		newBrightness = brightnessSlider.value;
+		Debug.Log(newBrightness);
+		RenderSettings.ambientLight = new Color(newBrightness, newBrightness, newBrightness, newBrightness);
+		Debug.Log(RenderSettings.ambientLight);
+	}
+}
