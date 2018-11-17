9d83eff943a7607cd8d4923a5d1565c09c6b4f1e
Aria Myers <jsm3767@g.rit.edu>
2018-11-17 03:27:24 -0500

    Fixed Multiple Block Problem
    I had to change the way push potions are scripted so that it is more readable, and I used joints to connect the wooden blocks so that now the larger block does not need a collider.  Also, The push potion should just apply a force on everything, becuase anything that shouldn't move wont have a rigidbody.

--- AetherAlchemy/Assets/Scripts/PotionScripts/PushEffect.cs
+++ AetherAlchemy/Assets/Scripts/PotionScripts/PushEffect.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class PushEffect : MonoBehaviour {
 
     float timer;
-    public float pushScalar;
-	// Use this for initialization
-	void Start () {
+    [SerializeField] float pushScalar;
+    [SerializeField] float radius = 1;
+
+    // Use this for initialization
+    void Start () {
+        SphereCollider coll = GetComponent<SphereCollider>();
+        coll.radius = radius;
         timer = 1.0f;
 	}
 	
@@ -24,23 +28,17 @@ public class PushEffect : MonoBehaviour {
     {
         if (other.gameObject.tag == "Movable" || other.gameObject.tag == "Player")
         {
-            Vector3 pushForce = new Vector3();
-
-            pushForce.x = other.gameObject.transform.position.x - transform.position.x;
-            pushForce.y = other.gameObject.transform.position.y - transform.position.y;
-            pushForce.z = other.gameObject.transform.position.z - transform.position.z;
-
-            float magnitude = Vector3.Magnitude(pushForce);
-
-            pushForce.Normalize();
-            pushForce = pushForce * pushScalar / magnitude;
+            Vector3 pushForce = other.gameObject.transform.position - transform.position;
+            float distance = pushForce.magnitude;
+            pushForce = pushForce.normalized;
+            pushForce *= (pushScalar * (distance / radius));
 
 			if (other.gameObject.tag == "Player") {
-				other.gameObject.GetComponent<Rigidbody> ().velocity = pushForce/5;
+				other.gameObject.GetComponent<Rigidbody>().velocity = pushForce;
 			} 
 			else 
 			{
-				other.gameObject.GetComponent<Rigidbody>().AddForce(pushForce, ForceMode.Impulse);
+				other.gameObject.GetComponent<Rigidbody>().velocity = pushForce;
 			}
         }
     }
