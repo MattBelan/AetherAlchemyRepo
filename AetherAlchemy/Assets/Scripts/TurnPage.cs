c791678c25dafbbbc58e66c391f637b3e47dda2a
LeoHolman <lah2719@g.rit.edu>
2018-12-11 18:08:52 -0500

    Dummy save panel appears onclicking save game
    Returns to main menu on cancel

--- AetherAlchemy/Assets/Scripts/TurnPage.cs
+++ AetherAlchemy/Assets/Scripts/TurnPage.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TurnPage : MonoBehaviour {
 
 	public GameObject currentPage;
 	public GameObject nextPage;
+	// public GameObject panelsClosingLeft;
+	// public GameObject panelsClosingRight;
+	// public GameObject panelsOpeningLeft;
+	// public GameObject panelsOpeningRight;
 
 	public void TurnPages() {
 		currentPage.SetActive(false);
@@ -19,4 +24,11 @@ public class TurnPage : MonoBehaviour {
 	public void SubMenuTurnBackward() {
 		currentPage.SetActive(false);
 	}
+	//
+	// public void OpenSavePanel() {
+	// 	panelsClosingLeft.SetActive(false);
+	// 	panelsClosingRight.SetActive(false);
+	// 	panelsOpeningLeft.SetActive(true);
+	// 	panelsOpeningRight.SetActive(true);
+	// }
 }
